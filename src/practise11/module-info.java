c79993c1f0203334bf35c36ee3285c89eb487952
rintaro-o-dev <rintaro.okawa@oitoma-ro.com>
2026-05-17 10:49:57 +0900

    Update Practise : module commands practise

--- src/practise11/module-info.java
+++ src/practise11/module-info.java
@@ -22,6 +22,7 @@ module practise11 {
 
 // 情報隠蔽 : アプリケーションがどのようなパッケージやクラスで成り立っているかを隠蔽する
 // カプセル化とは違う！（データ隠蔽は関係ない）
+
 // 実行
 // java --module-path lib -m practise11/com.emsample.p1.Hello
 
@@ -36,3 +37,63 @@ module practise11 {
 // opens … to	特定モジュールにだけリフレクション公開
 // uses	サービス利用
 // provides … with	サービス実装提供
+
+// モジュールのアクセス関係
+// A : 名前付きモジュール ...module-info.java でモジュール宣言しているモジュール
+// B : 自動モジュール ...モジュールパス上に配置されたモジュール
+// C : 無名モジュール ...クラスパス上に配置されたモジュール
+//  ↓
+// 参照できるモジュール
+// A -> B
+// B -> A(--add-modules が必要) , C
+// C -> A(--add-modules が必要) , B(--add-modules が必要)
+
+// export されていないモジュールを一時的に公開する(緊急的な用途)
+// javac ... --add-exports foo(対象モジュール)/com.test(公開したいパッケージ)=hello(利用するモジュール)
+
+// module-info.java に設定したモジュールの設定情報を調べる
+// java --describe-mojule
+// jmod describe
+
+// requires transitive の利用
+// A -> B -> C の参照関係がある時、 B で requires transitive C とすると、
+// A でも Cが使えるようになる (B を参照するモジュールは C が使えるようになる)
+
+// モジュール移行アプローチ
+// ボトムアップ移行 ...最も依存"されている"モジュール(末端)から名前付きモジュールに移行していく
+// トップダウン移行 ...依存"している"モジュールから名前付きモジュールに移行していく
+// └─無名モジュールは一時的に自動モジュールに変換しておくのが大事 (名前付きモジュールは無名モジュールを参照できない！)
+
+// App モジュールが lib モジュールの参照をしっかり定義していれば、
+// java --module-path project/module --module App(モジュール名)/Main(完全修飾クラス名)
+// でいい
+
+// java.util.ServiceLoader
+// ServiceLoader（SPI）は “プラグイン方式” を実現する仕組み。
+//
+// SPI を実現するためのクラス -> アプリとライブラリをインターフェースで繋げることでライブラリの改修によるアプリ側への影響を抑えるモデル
+// META-INF/service 内のファイルを検索する
+// load メソッドには class リテラルを渡す
+// クラスファイルをロードするためのクラスではない！
+//
+// App モジュールの module-info.java
+// uses app.Hello; (app.Hello インタフェースを SPI として使う宣言)
+// lib モジュールの module-info.java
+// provides app.hell with lib.HelloImpl; (app.Hello インタフェースを実装しているのが lib.HelloImpl であることを宣言)
+
+// jdeps
+// jdeps は “依存関係と内部 API の検出” に使う。
+//
+// jdeps XX.jar
+//   └─ jar が依存しているモジュールやパッケージを確認できる
+// jdeps -verbose:class -cp lib/tools.jar com.sun.tools.jdeps.Main
+//   └─ jar のクラスレベルの依存を確認できるが、"JDK の内部APIを含まない"
+// jdeps -jdkinternals -cp lib/tools.jar com.sun.tools.jdeps.Main
+//   └─ jar のクラスレベルの依存を確認でき、"JDK の内部APIを含む"
+// jdeps -profile demo/jfc/XX.jar
+//   └─ jar が依存しているプロファイルを検索
+
+
+
+
+
